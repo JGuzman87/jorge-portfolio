0fca8626704a584c8be3461c1bab0072d76b8cbd
JGuzman87 <jguzman913@gmail.com>
2025-04-16 00:08:19 -0400

    fixed metadata

--- .next/static/chunks/src_cd6c7ea4._.js
+++ .next/static/chunks/src_cd6c7ea4._.js
@@ -163,7 +163,7 @@ const Portfolio = ()=>{
                     columnNumber: 9
                 }, this),
                 /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$client$2f$app$2d$dir$2f$link$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["default"], {
-                    href: "https://jguzman87.github.io/weather-dashboard/",
+                    href: "https://weather-dashboard-react-three.vercel.app",
                     children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
                         className: "card bg-base-100 w-80 shadow-sm justify-self-center self-center",
                         children: [
